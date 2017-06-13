11813a4afd7bf6d6d940a6d546c0cbddb7ab9f12
Tony Nassif <tonynassif@gmail.com>
2017-06-13 16:27:10 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,13 +4,19 @@
 # or whitespace (' ').
 class EmailParser
   attr_accessor :email
+  @@email_list = []
 
   def initialize(email_string)
-
+    email_string = self.new
+    email_list << email_string
   end
 
   def self.parse
     email_array = email_list.split(/[\s,]/)
     email_array
+  end
+
+  def self.email_list
+    @@email_list
   end
 end
